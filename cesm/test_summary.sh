055d06b5cf247e3fe40a19ff3599b0473ec9bc88
Sam Rabin <sam.rabin@gmail.com>
2023-08-30 16:19:27 -0600

    test_summary.sh: Account for pending tests.

--- cesm/test_summary.sh
+++ cesm/test_summary.sh
@@ -6,7 +6,10 @@ script="$(get_cs.status)"
 tmpfile=.test_summary.$(date "+%Y%m%d%H%M%S%N")
 ${script} > ${tmpfile}
 
+# We don't want the script to exit if grep finds no matches
 set +e
+
+# Account for completed tests
 grep -E "FAIL.*BASELINE.*DIFF" ${tmpfile} | awk '{print $2}' > accounted_for_truediffs
 grep -E "FAIL.*BASELINE exception" ${tmpfile} | awk '{print $2}' > accounted_for_baselineException
 grep -E "FAIL.*MODEL_BUILD" ${tmpfile} | awk '{print $2}' > accounted_for_modelBuild
@@ -17,11 +20,28 @@ grep -E "FAIL.*BASELINE.*some baseline files were missing" ${tmpfile} | awk '{pr
 grep -E "FAIL.*BASELINE.*baseline directory.*does not exist" ${tmpfile} | awk '{print $2}' > accounted_for_missingBaselineDir
 grep -E "EXPECTED FAILURE" ${tmpfile} | awk '{print $2}' > accounted_for_expectedFail
 grep -E "FAIL.*XML*" ${tmpfile} | awk '{print $2}' > accounted_for_xmlFail
+
+# Account for pending tests
+[[ -e accounted_for_pend ]] && rm accounted_for_pend
+for t in $(grep -E "Overall: PEND" ${tmpfile} | awk '{print $1}' | sort); do
+    if [[ $(grep $t accounted_for_expectedFail | wc -l) -eq 0 ]]; then
+        echo $t >> accounted_for_pend
+    fi
+done
+
 set -e
 
 for d in $(grep "Overall" ${tmpfile} | awk '{print $1}'); do [[ $(grep $d accounted_for* | wc -l) -eq 0 ]] && ${script} | grep $d; done > not_accounted_for
 
-for f in *accounted*; do echo $f; cat $f; echo " "; done
+for f in accounted*; do [[ $f == accounted_for_pend ]] && continue; echo $f; cat $f; echo " "; done
+
+# Print these last
+echo accounted_for_pend
+cat accounted_for_pend
+echo " "
+echo not_accounted_for
+cat not_accounted_for
+echo " "
 
 rm ${tmpfile}
 exit 0
