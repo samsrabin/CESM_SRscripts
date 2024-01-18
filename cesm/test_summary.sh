93420e3e52611f28024d3b48e02b6ec55a2c81e6
Sam Rabin <sam.rabin@gmail.com>
2024-01-18 11:52:58 -0700

    test_summary.sh: Handle fails in create_newcase.

--- cesm/test_summary.sh
+++ cesm/test_summary.sh
@@ -11,6 +11,7 @@ set +e
 
 # Account for completed tests
 grep -E "FAIL.*BASELINE exception" ${tmpfile} | awk '{print $2}' > accounted_for_baselineException
+grep -E "FAIL.*CREATE_NEWCASE" ${tmpfile} | grep -v "EXPECTED" | awk '{print $2}' > accounted_for_createCase
 grep -E "FAIL.*SHAREDLIB_BUILD" ${tmpfile} | grep -v "EXPECTED" | awk '{print $2}' > accounted_for_sharedlibBuild
 grep -E "FAIL.*MODEL_BUILD" ${tmpfile} | grep -v "EXPECTED" | awk '{print $2}' > accounted_for_modelBuild
 grep -E "FAIL.*RUN" ${tmpfile} | grep -v "EXPECTED" | awk '{print $2}' > accounted_for_runFail
