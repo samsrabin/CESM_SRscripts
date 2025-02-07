8e8c2531a4b3bb9bf9484733e926341197fbb0e4
Sam Rabin <sam.rabin@gmail.com>
2025-02-07 15:33:11 -0700

    check_svn_server: Better path handling.

--- cesm/check_svn_server.sh
+++ cesm/check_svn_server.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -e
 
-server_url="https://svn-ccsm-inputdata.cgd.ucar.edu/trunk/inputdata/lnd/clm2"
+inputdata_parent="/fs/cgd/csm/inputdata"
+server_url="https://svn-ccsm-inputdata.cgd.ucar.edu/trunk/inputdata"
 
 files_to_check="$@"
 
-cd /fs/cgd/csm/inputdata/lnd/clm2
+
 for f in ${files_to_check}; do
-    url=${server_url}/$f
+    # Make sure file is in the input data dir
+    f_abspath="$(realpath $f)"
+    if [[ "${f_abspath}" != "${inputdata_parent}"* ]]; then
+        echo "Skipping $f (not in ${inputdata_parent})" >&2
+        continue
+    fi
+
+    f_rel="$(realpath -s --relative-to="${inputdata_parent}" $f)"
+    url=${server_url}/$f_rel
     if curl --output /dev/null --silent --head --fail "$url"; then
         # I.e., file is on server
         continue
