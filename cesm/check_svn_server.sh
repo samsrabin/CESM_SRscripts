f39b4ab05fb6c2948b6aee7943e96543918b75bc
Sam Rabin <sam.rabin@gmail.com>
2025-02-07 15:23:19 -0700

    check_svn_server: Read list of files to check

--- cesm/check_svn_server.sh
+++ cesm/check_svn_server.sh
@@ -3,14 +3,16 @@ set -e
 
 server_url="https://svn-ccsm-inputdata.cgd.ucar.edu/trunk/inputdata/lnd/clm2"
 
+files_to_check="$@"
+
 cd /fs/cgd/csm/inputdata/lnd/clm2
-for f in $(find . -type f | sort); do
+for f in ${files_to_check}; do
     url=${server_url}/$f
     if curl --output /dev/null --silent --head --fail "$url"; then
+        # I.e., file is on server
         continue
-    else
-        echo $f
     fi
+    echo $f
 done
 
 exit 0
