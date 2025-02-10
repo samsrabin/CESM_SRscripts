ed8f0e706e4b676909c20ac7b0429558a06a0f0f
Sam Rabin <sam.rabin@gmail.com>
2025-02-10 15:30:28 -0700

    ping_svn_server: Exit after 1 successful ping

--- cesm/ping_svn_server.sh
+++ cesm/ping_svn_server.sh
@@ -4,6 +4,6 @@ set -e
 timeout=10  # seconds
 server_url="svn-ccsm-inputdata.cgd.ucar.edu"
 
-ping -w ${timeout} "$server_url" 1>/dev/null 2>&1
+ping -c 1 -w ${timeout} "$server_url" 1>/dev/null 2>&1
 
 exit 0
